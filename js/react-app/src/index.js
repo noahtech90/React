6c91734c593170fc487e0977a1d81b9830c57ea7
Noah <noahhodge2@gmail.com>
2021-05-08 15:47:42 -0700

    mutiple tags

--- js/react-app/src/index.js
+++ js/react-app/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './componets/counter'
 
-const element = <h1>Hello World</h1>
-ReactDOM.render(element, document.getElementById('root'))
+
+ReactDOM.render(<Counter/>, document.getElementById('root'))
 
 
 // If you want to start measuring performance in your app, pass a function
