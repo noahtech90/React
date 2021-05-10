c17ab692809f45b5867f2fa81bb2a3636b964c29
Noah <noahhodge2@gmail.com>
2021-05-09 17:28:17 -0700

    gitignore

--- js/react-app/src/componets/counter.jsx
+++ js/react-app/src/componets/counter.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0,
+        today: new Date().toLocaleDateString(),
+        imageUrl: "https://picsum.photos/200"
+     };
+
+     dateStyles = {
+        margin: 4,
+        fontWeight: 'bold',
+        display: 'block'
+     };
+
+     imgStyles = {
+         display: 'block',
+         marginTop: 200,
+         marginLeft: '40%'
+
+     }
+
     render() { 
         return (
         <div>
-            <h1>Hello World</h1>
-            <button>Increment</button>
+            <span style={ this.dateStyles }>{this.formatDate()}</span>
+            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            <img style={this.imgStyles} src={this.state.imageUrl} alt="" />
         </div>);
+    }
+
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? "Zero" : count
+    }
+
+    formatDate() {
+        const { today } = this.state
+        return today
     }
 }
  
