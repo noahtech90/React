0706b38307072792f7f6d13f34649b371c1a53ca
Noah <noahhodge2@gmail.com>
2021-05-12 19:37:58 -0700

    conditional

--- js/react-app/src/componets/counter.jsx
+++ js/react-app/src/componets/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
     state = {
         count: 1,
         today: new Date().toLocaleDateString(),
-        imageUrl: "https://picsum.photos/200"
+        imageUrl: "https://picsum.photos/200",
+        tags: ["tag1", "tag2", "tag3"]
      };
 
      dateStyles = {
@@ -20,14 +21,24 @@ class Counter extends Component {
 
      }
 
+    renderTags() {
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+       return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>; 
+    }
+
     render() { 
 
         return (
         <div>
+            { this.state.tags.length === 0 && 'Please create a new tag'}
+
             <span style={ this.dateStyles }>{this.formatDate()}</span>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
             <img style={this.imgStyles} src={this.state.imageUrl} alt="" />
+            { this.renderTags() }
+
         </div>);
     }
 
