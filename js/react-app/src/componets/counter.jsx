570fcf64690140cd298069162614b8d87ab7666d
Noah <noahhodge2@gmail.com>
2021-05-12 19:19:49 -0700

    style

--- js/react-app/src/componets/counter.jsx
+++ js/react-app/src/componets/counter.jsx
@@ -23,6 +23,7 @@ class Counter extends Component {
     render() { 
         return (
         <div>
+            <h2 style={{fontSize: 100}}>Howdy</h2>
             <span style={ this.dateStyles }>{this.formatDate()}</span>
             <span className="badge badge-primary m-2">{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
