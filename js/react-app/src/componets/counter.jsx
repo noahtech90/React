d2f2c616c041b7b6302c481157133d10f88cd9d7
Noah <noahhodge2@gmail.com>
2021-05-12 19:26:39 -0700

    dynamic class

--- js/react-app/src/componets/counter.jsx
+++ js/react-app/src/componets/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
         today: new Date().toLocaleDateString(),
         imageUrl: "https://picsum.photos/200"
      };
@@ -21,14 +21,20 @@ class Counter extends Component {
      }
 
     render() { 
+
         return (
         <div>
-            <h2 style={{fontSize: 100}}>Howdy</h2>
             <span style={ this.dateStyles }>{this.formatDate()}</span>
-            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
             <img style={this.imgStyles} src={this.state.imageUrl} alt="" />
         </div>);
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
