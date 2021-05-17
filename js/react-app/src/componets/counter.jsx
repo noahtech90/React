c42b19ceb2c98b7a76d5ac32e958de4e9649b66e
Noah <noahhodge2@gmail.com>
2021-05-16 19:44:33 -0700

    set state

--- js/react-app/src/componets/counter.jsx
+++ js/react-app/src/componets/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 1,
+        count: 0,
         today: new Date().toLocaleDateString(),
         imageUrl: "https://picsum.photos/200",
         tags: ["tag1", "tag2", "tag3"]
@@ -27,15 +27,18 @@ class Counter extends Component {
        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>; 
     }
 
+    handleIncrement = () => {
+        this.setState(count++)
+    }
+
     render() { 
 
         return (
         <div>
             { this.state.tags.length === 0 && 'Please create a new tag'}
-
             <span style={ this.dateStyles }>{this.formatDate()}</span>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             <img style={this.imgStyles} src={this.state.imageUrl} alt="" />
             { this.renderTags() }
 
