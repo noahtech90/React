4b2e00e0a3c380ddd0e858a4b009f3bf525a4a54
Noah <noahhodge2@gmail.com>
2021-05-23 15:05:23 -0700

    source of truth

--- js/react-app/src/componets/counter.jsx
+++ js/react-app/src/componets/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: this.props.value,
+        count: this.props.counter.value,
         today: new Date().toLocaleDateString(),
         imageUrl: "https://picsum.photos/200",
         tags: ["tag1", "tag2", "tag3"]
@@ -38,6 +38,7 @@ class Counter extends Component {
             { this.state.tags.length === 0 && 'Please create a new tag'}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={() => {this.handleIncrement()}} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>);
     }
 
